fedf8144d24384200656f50c0d63fb1fc27b8c5c
victorpena13 <victor.pena1392@gmail.com>
2019-08-20 15:04:22 -0500

    HelloWorld completed

--- src/main/java/HelloWorldServlet.java
+++ src/main/java/HelloWorldServlet.java
@@ -1,9 +1,21 @@
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
 
+@WebServlet("/Hello")
 public class HelloWorldServlet extends HttpServlet {
-    protected void doGet(HttpServletRequest req, HttpServletResponse res) {
+    String message;
+    protected void doGet(HttpServletRequest req,
+                         HttpServletResponse res) throws ServletException, IOException {
+        PrintWriter out = res.getWriter();
+
+        message = "Hello World!!";
+        out.println("<h1>" + message + "</h1>");
 
     }
+
 }
