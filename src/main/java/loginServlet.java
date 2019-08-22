373750b973ce85424abdf5e6f7a2fa655332aee1
victorpena13 <victor.pena1392@gmail.com>
2019-08-22 09:03:47 -0500

    loginservlet version completed

--- src/main/java/loginServlet.java
+++ src/main/java/loginServlet.java
@@ -1,21 +1,30 @@
-import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-
-@WebServlet(name = "loginServlet" , urlPatterns = "/login" )
-public class loginServlet extends HttpServlet {
-    @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-
-        req.getRequestDispatcher("login.jsp").forward(req, resp);
-    }
-
-    @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-
-
-    }
-}
+//import javax.servlet.ServletException;
+//import javax.servlet.annotation.WebServlet;
+//import javax.servlet.http.HttpServlet;
+//import javax.servlet.http.HttpServletRequest;
+//import javax.servlet.http.HttpServletResponse;
+//import java.io.IOException;
+//
+//@WebServlet(name = "loginServlet" , urlPatterns = "/login" )
+//public class loginServlet extends HttpServlet {
+//    @Override
+//    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+//
+//        req.getRequestDispatcher("login.jsp").forward(req, resp);
+////        resp.sendRedirect("/login");
+//    }
+//
+//    @Override
+//    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+//        String username = req.getParameter("username");
+//        String password = req.getParameter("password");
+//        if(username.equalsIgnoreCase("admin") && password.equalsIgnoreCase("password")){
+//            req.setAttribute("nameuser", username);
+////            resp.sendRedirect("/profile");
+//            req.getRequestDispatcher("profile.jsp").forward(req, resp);
+//        } else {
+//            req.getRequestDispatcher("login.jsp").forward(req, resp);
+//        }
+//
+//    }
+//}
--- src/main/webapp/profile.jsp
+++ src/main/webapp/profile.jsp
@@ -5,7 +5,10 @@
   Time: 15:36
   To change this template use File | Settings | File Templates.
 --%>
+
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+
 <html>
 <head>
     <title>Title</title>
@@ -13,7 +16,7 @@
 <body>
 
 
-<h1>profile page !!</h1>
+<h1>profile page user: <%= request.getParameter("nameuser") %>!</h1>
 
 
 </body>
